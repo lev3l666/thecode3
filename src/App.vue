d9bcc216c6b1ca837584f2d2886e2218377aece7
Jairo Sierra <jairo.sierra@6connex.com>
2022-04-08 16:54:49 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,6 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Evelia" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style>
